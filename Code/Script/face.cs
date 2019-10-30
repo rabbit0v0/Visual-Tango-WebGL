381dfa85f92a746eb9146bd5f993395d00cd5d75
Rabbit0v0 <yy_2016@sjtu.edu.cn>
2019-10-30 17:23:11 -0400

    code

---
+++ Code/Script/face.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// what happens when you change your selections of the dropdowns
+// do not walk around and rotate
+
+public class face : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Facedir").GetComponent<Dropdown>().onValueChanged.AddListener(facedir);
+        GameObject.Find("Weighted").GetComponent<Dropdown>().onValueChanged.AddListener(weight);
+        GameObject.Find("Pose").GetComponent<Dropdown>().onValueChanged.AddListener(pose);
+        GameObject.Find("Position").GetComponent<Dropdown>().onValueChanged.AddListener(position);
+        //GameObject.Find("Rotate").GetComponent<Dropdown>().onValueChanged.AddListener(rotate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int dir = 0;
+    int wei = 0;
+    int hei = 0;
+    int pos = 0;
+    //int rot = 0;
+
+    // weighted leg pose
+    public void show()
+    {
+        GameObject woman = GameObject.Find("Spine");
+        Animator anis = woman.GetComponent<Animator>();
+        anis.SetInteger("face", dir);
+
+        GameObject leg_l = GameObject.Find("LeftUpLeg");
+        Animator ani = leg_l.GetComponent<Animator>();
+        GameObject leg_r = GameObject.Find("RightUpLeg");
+        Animator ani0 = leg_r.GetComponent<Animator>();
+        if (wei == 0) // right leg weighted
+        {
+            ani0.SetInteger("state", 0);
+            if (hei == 1) // bent
+            {
+                ani0.SetInteger("hi", 1);
+                ani.SetInteger("hi", 1);
+            }
+            else if (hei == 3) // tiptoe
+            {
+                ani0.SetInteger("hi", 2);
+                ani.SetInteger("hi", 0);
+            }
+            else // straight
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 0);
+            }
+            
+            ani.SetInteger("state", pos);
+
+
+        }
+        else
+        {
+            ani.SetInteger("state", 0);
+            if (hei == 1)
+            {
+                ani0.SetInteger("hi", 1);
+                ani.SetInteger("hi", 1);
+            }
+            else if (hei == 3)
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 2);
+            }
+            else
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 0);
+            }
+            
+            ani0.SetInteger("state", pos);
+            
+        }
+    }
+
+    // changing free leg pose
+    public void pose(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                pos = 2;
+                break;
+            case 1:
+                pos = 3;
+                break;
+            case 2:
+                pos = 1;
+                break;
+            case 3:
+                pos = 4;
+                break;
+            case 4:
+                pos = 5;
+                break;
+            case 5:
+                pos = 6;
+                break;
+            case 6:
+                pos = 7;
+                break;
+            case 7:
+                pos = 8;
+                break;
+            case 8:
+                pos = 9;
+                break;
+            case 9:
+                pos = 10;
+                break;
+        }
+        show();
+    }
+
+    // changing the height
+    public void position(int value)
+    {
+        GameObject model = GameObject.Find("dance");
+        Animator ani = model.GetComponent<Animator>();
+        GameObject leg_l = GameObject.Find("LeftUpLeg");
+        Animator ani1 = leg_l.GetComponent<Animator>();
+        GameObject leg_r = GameObject.Find("RightUpLeg");
+        Animator ani0 = leg_r.GetComponent<Animator>();
+        switch (value)
+        {
+            case 0:
+                //if (hei == 1)
+                ani.SetInteger("state", 2);
+                hei = 0;
+                ani0.SetInteger("hi", 0);
+                ani1.SetInteger("hi", 0);
+                break;
+            case 1:
+                ani.SetInteger("state", 1);
+                hei = 1;
+                ani0.SetInteger("hi", 1);
+                ani1.SetInteger("hi", 1);
+                break;
+            case 2:
+                ani.SetInteger("state", 3);
+                hei = 3;
+                ani0.SetInteger("hi", 2);
+                ani1.SetInteger("hi", 2);
+                break;
+        }
+        show();
+    }
+
+    // changing the weighted leg
+    public void weight(int value)
+    {
+        wei = value;
+        show();
+    }
+
+    //public void rotate(int value)
+    //{
+    //    rot = value;
+    //    //switch (value)
+    //    //{
+    //    //    case 0:
+    //    //        rot = 0;
+    //    //        break;
+    //    //    case 1:
+    //    //        rot = 1;
+    //    //        break;
+
+    //    //}
+    //    show(); 
+    //}
+
+    // changing the face direction
+    public void facedir(int value)
+    {
+        GameObject woman = GameObject.Find("Spine");
+        Animator ani = woman.GetComponent<Animator>();
+       
+        switch (value)
+        {
+            case 0:
+                //if (dir == 1)
+                //    ani.SetInteger("face", 4);
+                //else if (dir == 2)
+                //    ani.SetInteger("face", 2);
+                dir = 0;
+                break;
+            case 1:
+                //if (dir == 0)
+                //    ani.SetInteger("face", 3);
+                dir = 1;
+                break;
+            case 2:
+                //if (dir == 0)
+                //    ani.SetInteger("face", 1);
+                dir = 2;
+                break;
+        }
+        show();
+    }
+}
---
+++ Code/Script/save.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// a class which keeps all the information for each step
+public class Pose
+{
+    public int d = 0;
+    public int w = 0;
+    public int p = 0;
+    public int h = 0;
+    public int t = 2;
+    public int r = 0;
+
+    public void init(int dd = 0, int ww = 0, int pp = 0, int hh = 0, int tt = 0, int rr = 0)
+    {
+        d = dd;
+        w = ww;
+        p = pp;
+        h = hh;
+        t = tt;
+        r = rr;
+    }
+}
+
+// a list that keeps all the poses
+public static class streaming
+{
+    public static List<Pose> l = new List<Pose>();
+    public static List<Pose> list
+    {
+        get
+        {
+            return l;
+        }
+        set
+        {
+            streaming.l = value;
+        }
+    }
+}
+
+// keep the number of the poses
+public static class total
+{
+    public static int t = 0;
+    public static int tot
+    {
+        get
+        {
+            return t;
+        }
+        set
+        {
+            total.t = value;
+        }
+    }
+}
+
+// tell which pose is selected by clicking on the logs in the right part of the screen (to delete and insert)
+public static class selected
+{
+    public static int s = 0;
+    public static int select
+    {
+        get
+        {
+            return s;
+        }
+        set
+        {
+            selected.s = value;
+        }
+    }
+}
+
+// what happens when "save" is clicked
+// button is going to change its name
+public class save : MonoBehaviour
+{
+    string[] Name = { "Collected", "Corssed forward", "Forward", "Backward", "In air forward", "In air backward", "Slide outside", "Wrapped around", "Collected high", "Crossed backward" };
+    string[] Height = { "straight", "bent", "tiptoe" };
+    string[] Leg = { "right", "left" };
+    string[] Direction = { "north", "northwest", "northeast" };
+    int[] angle = { 0, 30, 60, 90, 120, 150, 180, 270, 360 };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        Button button = gameObject.GetComponent<Button>() as Button;
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        int d = GameObject.Find("Facedir").GetComponent<Dropdown>().value;
+        int w = GameObject.Find("Weighted").GetComponent<Dropdown>().value;
+        int p = GameObject.Find("Pose").GetComponent<Dropdown>().value;
+        int h = GameObject.Find("Position").GetComponent<Dropdown>().value;
+        int r = angle[GameObject.Find("Rotate").GetComponent<Dropdown>().value];
+        int t = (int)GameObject.Find("Slider").GetComponent<Slider>().value;
+
+        Toggle rotate_dir = GameObject.Find("rotate_dir").GetComponent<Toggle>();
+        if (!rotate_dir.isOn)
+            r = -r;
+
+        Pose tmp = new Pose();
+        // save the pose
+        tmp.init(d, w, p, h, t, r);
+        List<Pose> tmplist = new List<Pose>();
+        // for insertion we need to have a new list for the poses
+        for (int i = 0; i < selected.select; ++i)
+        {
+            tmplist.Add(streaming.l[i]);
+        }
+        tmplist.Add(tmp);
+        for (int i = selected.select; i < total.tot; ++i)
+        {
+            tmplist.Add(streaming.l[i]);
+        }
+        streaming.list = tmplist;
+        total.tot += 1;
+        // after the insertion, cancel the clicked status and select the empty space after the last pose
+        selected.select = total.tot;
+        List<Pose> l = streaming.l;
+        GameObject canvas = GameObject.Find("Canvas");
+        // destroy all the old logs
+        foreach (GameObject bs in GameObject.FindGameObjectsWithTag("log"))
+        {
+            Destroy(bs);
+        }
+        // create new logs
+        for (int i = 0; i < total.t; ++i)
+        {
+            int index = i;
+            GameObject button = new GameObject("Button", typeof(Button), typeof(RectTransform), typeof(Text));
+            button.transform.SetParent(canvas.transform);
+            Button B = button.GetComponent<Button>();
+            B.tag = "log";
+            B.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 20);
+            B.GetComponent<RectTransform>().anchoredPosition = new Vector2(500, 300 - 50 * i);
+            //B.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+            
+
+            GameObject temp = new GameObject("Text", typeof(RectTransform), typeof(Text));
+            temp.transform.SetParent(button.transform);
+            Text text = temp.GetComponent<Text>();
+            text.tag = "text0";
+            text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+            text.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 20);
+            //text.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+            //text.GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
+            //text.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+            
+            text.text = "";
+            text.text += Name[l[i].p];
+            text.text += ", ";
+            text.text += Height[l[i].h];
+            text.text += ", ";
+            text.text += Leg[l[i].w];
+            text.text += ", ";
+            text.text += Direction[l[i].d];
+            text.text += ", ";
+            text.text += (l[i].t).ToString();
+            text.text += ", ";
+            text.text += (l[i].r).ToString();
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            text.fontSize = 14;
+            text.fontStyle = FontStyle.Normal;
+            text.color = Color.black;
+            //text.text += '\n';
+            B.onClick.AddListener(delegate () {// if the log is clicked, the text would be bold and select this log
+                selected.select = index;
+                foreach (GameObject ts in GameObject.FindGameObjectsWithTag("text0"))
+                {
+                    ts.GetComponent<Text>().fontStyle = FontStyle.Normal;
+                }
+                text.fontStyle = FontStyle.BoldAndItalic;
+                //Debug.Log(selected.select);
+            });
+            
+        }
+    }
+
+}
