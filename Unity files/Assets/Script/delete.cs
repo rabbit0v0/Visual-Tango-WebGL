c7fe73b65ccf81fc77e7c5f905187cde0ade5898
Rabbit0v0 <yy_2016@sjtu.edu.cn>
2019-11-04 20:01:09 +0800

    unity files

---
+++ Unity files/Assets/Script/delete.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// happens when delete is clicked
+public class delete : MonoBehaviour
+{
+    string[] Name = { "Collected", "Corssed forward", "Forward", "Backward", "In air forward", "In air backward", "Slide outside", "Wrapped around", "Collected high", "Crossed backward" };
+    string[] Height = { "straight", "bent", "tiptoe" };
+    string[] Leg = { "right", "left" };
+    string[] Direction = { "north", "northwest", "northeast" };
+    GameObject dialog;
+    // Start is called before the first frame update
+    // when start, do not show the dialog ("are you sure to delete xxx")
+    void Start()
+    {
+        dialog = GameObject.Find("Dialog");
+        dialog.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // add listener
+    void Awake()
+    {
+        Button button = gameObject.GetComponent<Button>() as Button;
+        button.onClick.AddListener(makesure);
+        Button yes = GameObject.Find("de").GetComponent<Button>();
+        Button no = GameObject.Find("no").GetComponent<Button>();
+        yes.onClick.AddListener(OnClick);
+        no.onClick.AddListener(offClick);
+    }
+    // when "delete" is clicked, show the dialog
+    void makesure()
+    {
+        dialog.SetActive(true);
+        Text notice = GameObject.Find("sure").GetComponent<Text>();
+        int index = selected.select;
+        if (index == total.t)
+        {
+            index = index - 1;
+        }
+        notice.text = "Are you going to delete \"";
+        notice.text += Name[streaming.l[index].p];
+        notice.text += ", ";
+        notice.text += Height[streaming.l[index].h];
+        notice.text += ", ";
+        notice.text += Leg[streaming.l[index].w];
+        notice.text += ", ";
+        notice.text += Direction[streaming.l[index].d];
+        notice.text += ", ";
+        notice.text += (streaming.l[index].t).ToString();
+        notice.text += ", ";
+        notice.text += (streaming.l[index].r).ToString();
+        notice.text += "\"?";
+    }
+
+    // when click cancel, hide the dialog
+    // there is also a "cancle.cs" which makes the text to be normal style
+    void offClick()
+    {
+        dialog.SetActive(false);
+    }
+
+    // when click yes, delete the selected one
+    void OnClick()
+    {
+        dialog.SetActive(false);
+        //streaming.l.RemoveAt(streaming.l.Count - 1);
+        int ind = selected.select;
+        if (ind == total.t)
+        {
+            ind = ind - 1;
+        }
+        // reform the list
+        List<Pose> tmplist = new List<Pose>();
+        for (int i = 0; i < ind && i < total.tot; ++i)
+        {
+            tmplist.Add(streaming.l[i]);
+        }
+        for (int i = ind + 1; i < total.tot; ++i)
+        {
+            tmplist.Add(streaming.l[i]);
+        }
+        streaming.list = tmplist;
+        total.t-=1;
+        if (total.t < 0)
+            total.t = 0;
+        // select the empty space after the last pose
+        selected.select = total.tot;
+        GameObject canvas = GameObject.Find("Canvas");
+        List<Pose> l = streaming.l;
+        // rewrite the logs
+        foreach (GameObject bs in GameObject.FindGameObjectsWithTag("log"))
+        {
+            Destroy(bs);
+        }
+        for (int i = 0; i < total.t; ++i)
+        {
+            int index = i;
+            GameObject button = new GameObject("Button", typeof(Button), typeof(RectTransform), typeof(Text));
+            button.transform.SetParent(canvas.transform);
+            Button B = button.GetComponent<Button>();
+            B.tag = "log";
+            B.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 20);
+            B.GetComponent<RectTransform>().anchoredPosition = new Vector2(500, 300 - 50 * i);
+            //B.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
+
+            GameObject temp = new GameObject("Text", typeof(RectTransform), typeof(Text));
+            temp.transform.SetParent(button.transform);
+            Text text = temp.GetComponent<Text>();
+            text.tag = "text0";
+            text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+            text.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 20);
+
+            text.text = "";
+            text.text += Name[l[i].p];
+            text.text += ", ";
+            text.text += Height[l[i].h];
+            text.text += ", ";
+            text.text += Leg[l[i].w];
+            text.text += ", ";
+            text.text += Direction[l[i].d];
+            text.text += ", ";
+            text.text += (l[i].t).ToString();
+            text.text += ", ";
+            text.text += (l[i].r).ToString();
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            text.fontSize = 14;
+            text.fontStyle = FontStyle.Normal;
+            text.color = Color.black;
+            //text.text += '\n';
+            B.onClick.AddListener(delegate () {
+                selected.select = index;
+                foreach (GameObject ts in GameObject.FindGameObjectsWithTag("text0"))
+                {
+                    ts.GetComponent<Text>().fontStyle = FontStyle.Normal;
+                }
+                text.fontStyle = FontStyle.BoldAndItalic;
+                //Debug.Log(selected.select);
+            });
+
+        }
+    }
+}
---
+++ Unity files/Assets/Script/play.cs
@@ -0,0 +1,323 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// "isMoving" class is defined in around.cs
+/// "streaming" class, "total" class are definedinsave.cs
+/// "isTurning" class is defined in Turn.cs, which actually does the turning.
+/// </summary>
+
+// an index to control which pose to play
+public static class ind
+{
+    public static int i = -1;
+}
+
+// what happens when "play" button (or step) is clicked
+public class play : MonoBehaviour
+{
+    int status = 0;
+    GameObject stepback;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stepback = GameObject.Find("stepback");
+        stepback.SetActive(false);
+    }
+
+    // start playing
+    void FixedUpdate()
+    {
+        Text text = gameObject.GetComponent<Button>().GetComponentInChildren<Text>();
+        Toggle step = GameObject.Find("Toggle").GetComponent<Toggle>();
+        // to see whether we should play step by step or not.
+        if (step.isOn)
+        {
+            text.text = "step";
+            stepback.SetActive(true);
+        }
+        else
+        {
+            text.text = "play";
+            stepback.SetActive(false);
+        }
+        // play all in a row then stop.
+        if (status == 1)
+        {
+            if (ind.i == streaming.l.Count - 1)
+            {
+                go(ind.i);
+                ind.i = -1;
+                status = 0;
+                return;
+            }
+            ind.i = 1 + ind.i;
+            go(ind.i);
+        }
+        // step by step
+        else if (status == 2)
+        {
+            //step forward
+            if (!stepback.GetComponent<Toggle>().isOn)
+            {
+                ind.i = ind.i + 1;
+                if (ind.i == -1 || ind.i == streaming.l.Count)
+                {
+                    ind.i = -1;
+                    go(-1);
+                }
+                go(ind.i);
+                
+                status = 0;
+            }
+            // step back
+            else
+            {
+                ind.i = ind.i - 1;
+                if (ind.i == -1 || ind.i == streaming.l.Count)
+                {
+                    ind.i = streaming.l.Count - 1;
+                    go(-1);
+                }
+                go(ind.i);
+                Debug.Log(ind.i);
+                status = 0;
+            }
+        }
+    }
+    // add the listener to the button
+    void Awake()
+    {
+        Button button = gameObject.GetComponent<Button>() as Button;
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        Toggle step = GameObject.Find("Toggle").GetComponent<Toggle>();
+        if (step.isOn)
+            status = 2;// play step by step
+        else
+            status = 1;// play in a row
+    }
+    // make the pose
+    void go(int k)
+    {
+        if (k == -1)// for the last step, if there is any movement, do it.
+        {
+            Pose pp = streaming.l[total.tot-1];
+            stepAround(pp);
+            return;
+        }
+        
+        Move(streaming.l[k]);
+        stepAround(streaming.l[k]);
+        turnAround(streaming.l[k]);
+    }
+
+    // rotate
+    void turnAround(Pose p)
+    {
+        isTurning.r = p.r;
+    }
+
+    int pre = 0;
+    int pre_w = 0;
+
+    // calculate how far the women moves totally
+    // "isMoving" class is defined in around.cs
+    void stepAround(Pose p)
+    {
+        GameObject model = GameObject.Find("dance");
+        GameObject leg_r = GameObject.Find("RightUpLeg");
+        GameObject leg_rr = GameObject.Find("RightLeg");
+        GameObject leg_l = GameObject.Find("LeftUpLeg");
+        GameObject leg_ll = GameObject.Find("LeftLeg");
+        if (pre_w == 0) // in the former pose, she weighted on the right leg
+        {
+            if (p.w == 1)// change the weight to the left leg, so move to where the left feet is
+            {
+                isMoving.m = 1;
+                isMoving.tmp = model.transform.position;
+                isMoving.xx = (float)(Mathf.Sin(leg_l.transform.rotation.x) * 0.95 + Mathf.Sin(leg_ll.transform.rotation.x) * 0.45);
+                isMoving.zz = (float)(-Mathf.Sin(leg_l.transform.rotation.z) * 0.95 - Mathf.Sin(leg_ll.transform.rotation.z) * 0.45);
+            }
+            else
+                return;
+        }
+        else // in the former pose, she weighted on the left leg
+        {
+            if (p.w == 0) // change the weight to the right leg
+            {
+                isMoving.m = 1;
+                isMoving.tmp = model.transform.position;
+                isMoving.xx = (float)(Mathf.Sin(leg_r.transform.rotation.x) * 0.95 + Mathf.Sin(leg_rr.transform.rotation.x) * 0.45);
+                isMoving.zz = (float)(-Mathf.Sin(leg_r.transform.rotation.z) * 0.95 - Mathf.Sin(leg_rr.transform.rotation.z) * 0.45);
+                //model.transform.position = tmp + new Vector3((float)(-Mathf.Sin(leg_r.transform.rotation.x) * 0.95 - Mathf.Sin(leg_rr.transform.rotation.x) * 0.5), 0, (float)(-Mathf.Sin(leg_r.transform.rotation.z) * 0.95 - Mathf.Sin(leg_rr.transform.rotation.z) * 0.5));
+            }
+            else
+                return;
+        }
+        pre_w = p.w;
+    }
+
+    // change the pose
+    void Move(Pose p)
+    {
+        GameObject model = GameObject.Find("dance");
+        GameObject leg_r = GameObject.Find("RightUpLeg");
+        GameObject leg_l = GameObject.Find("LeftUpLeg");
+        Animator ani = leg_l.GetComponent<Animator>();
+        Animator ani0 = leg_r.GetComponent<Animator>();
+
+        // change timescale according to the "speed" sliders
+        // if timescale is 2, the timer is 2 times faster, while when timescale is 0.5, the timer is 2 times slower.
+        Time.timeScale = p.t + 0.50f;
+        int overall = (int)GameObject.Find("overAllTime").GetComponent<Slider>().value;
+        Time.timeScale = Time.timeScale * overall * 0.10f;
+        //Debug.Log(Time.timeScale);
+        if (p.w == 0) // weighted: right leg
+        {
+            ani0.SetInteger("state", 0);
+            if (p.h == 1) // height: bend
+            {
+                ani0.SetInteger("hi", 1);
+                ani.SetInteger("hi", 1);
+            }
+            else if (p.h == 2) // height: tiptoe
+            {
+                ani0.SetInteger("hi", 2);
+                ani.SetInteger("hi", 0);
+            }
+            else // height: straight
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 0);
+            }
+            switch (p.p) // free leg pose
+            {
+                case 0:
+                    ani.SetInteger("state", 2);
+                    break;
+                case 1:
+                    ani.SetInteger("state", 3);
+                    break;
+                case 2:
+                    ani.SetInteger("state", 1);
+                    break;
+                case 3:
+                    ani.SetInteger("state", 4);
+                    break;
+                case 4:
+                    ani.SetInteger("state", 5);
+                    break;
+                case 5:
+                    ani.SetInteger("state", 6);
+                    break;
+                case 6:
+                    ani.SetInteger("state", 7);
+                    break;
+                case 7:
+                    ani.SetInteger("state", 8);
+                    break;
+                case 8:
+                    ani.SetInteger("state", 9);
+                    break;
+                case 9:
+                    ani.SetInteger("state", 10);
+                    break;
+            }
+
+        }
+        else
+        {
+            ani.SetInteger("state", 0);
+            if (p.h == 1)
+            {
+                ani0.SetInteger("hi", 1);
+                ani.SetInteger("hi", 1);
+            }
+            else if (p.h == 2)
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 2);
+            }
+            else
+            {
+                ani0.SetInteger("hi", 0);
+                ani.SetInteger("hi", 0);
+            }
+            switch (p.p)
+            {
+                case 0:
+                    ani0.SetInteger("state", 2);
+                    break;
+                case 1:
+                    ani0.SetInteger("state", 3);
+                    break;
+                case 2:
+                    ani0.SetInteger("state", 1);
+                    break;
+                case 3:
+                    ani0.SetInteger("state", 4);
+                    break;
+                case 4:
+                    ani0.SetInteger("state", 5);
+                    break;
+                case 5:
+                    ani0.SetInteger("state", 6);
+                    break;
+                case 6:
+                    ani0.SetInteger("state", 7);
+                    break;
+                case 7:
+                    ani0.SetInteger("state", 8);
+                    break;
+                case 8:
+                    ani0.SetInteger("state", 9);
+                    break;
+                case 9:
+                    ani0.SetInteger("state", 10);
+                    break;
+            }
+
+        }
+        GameObject woman = GameObject.Find("Spine");
+        Animator ani1 = woman.GetComponent<Animator>();
+        switch (p.d) // the face direction
+        {
+            case 0:
+                ani1.SetInteger("face", 0);
+
+                break;
+            case 1:
+                ani1.SetInteger("face", 1);
+
+                break;
+            case 2:
+                ani1.SetInteger("face", 2);
+
+                break;
+        }
+        Animator ani2 = model.GetComponent<Animator>();
+        switch (p.h) // the height
+        {
+            case 0:
+                ani2.SetInteger("state", 2);
+                //ani0.SetInteger("hi", 0);
+                //ani.SetInteger("hi", 0);
+                break;
+            case 1:
+                ani2.SetInteger("state", 1);
+                //ani0.SetInteger("hi", 1);
+                //ani.SetInteger("hi", 1);
+                break;
+            case 2:
+                ani2.SetInteger("state", 3);
+                //ani0.SetInteger("hi", 2);
+                //ani.SetInteger("hi", 2);
+                break;
+        }
+    }
+}
